be1b3ed7e06500960190b10dbb797c33cca3e6f4
moisturizedpotato <ritesh.mishra15jul@gmail.com>
2026-06-10 20:21:39 +0530

    removed the cube

--- src/main.js
+++ src/main.js
@@ -46,11 +46,6 @@ const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize( sizes.width, sizes.height );
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 gltfLoader.load("/models/cabin-v4.glb", (glb) =>{
   glb.scene.traverse((child) => {
     if (child.isMesh) {
